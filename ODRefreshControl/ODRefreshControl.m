cc0de27806b7f97286b1c6cf48b27f26efd2400a
sho yakushiji <sho.yakushiji@gmail.com>
2014-05-24 20:34:34 +0900

    Custom shape layer path

--- ODRefreshControl/ODRefreshControl.m
+++ ODRefreshControl/ODRefreshControl.m
@@ -14,10 +14,10 @@
 #define kOpenedViewHeight   44
 #define kMinTopPadding      9
 #define kMaxTopPadding      5
-#define kMinTopRadius       12.5
-#define kMaxTopRadius       16
-#define kMinBottomRadius    3
-#define kMaxBottomRadius    16
+#define kMinTopRadius       6
+#define kMaxTopRadius       10
+#define kMinBottomRadius    1
+#define kMaxBottomRadius    10
 #define kMinBottomPadding   4
 #define kMaxBottomPadding   6
 #define kMinArrowSize       2
@@ -68,9 +68,6 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         _activity.center = CGPointMake(floor(self.frame.size.width / 2), floor(self.frame.size.height / 2));
         _activity.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleRightMargin;
         _activity.alpha = 0;
-        if ([_activity respondsToSelector:@selector(startAnimating)]) {
-            [(UIActivityIndicatorView *)_activity startAnimating];
-        }
         [self addSubview:_activity];
         
         _refreshing = NO;
@@ -85,10 +82,6 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         _shapeLayer.fillColor = [_tintColor CGColor];
         _shapeLayer.strokeColor = [[[UIColor darkGrayColor] colorWithAlphaComponent:0.5] CGColor];
         _shapeLayer.lineWidth = 0.5;
-        _shapeLayer.shadowColor = [[UIColor blackColor] CGColor];
-        _shapeLayer.shadowOffset = CGSizeMake(0, 1);
-        _shapeLayer.shadowOpacity = 0.4;
-        _shapeLayer.shadowRadius = 0.5;
         [self.layer addSublayer:_shapeLayer];
         
         _arrowLayer = [CAShapeLayer layer];
@@ -148,27 +141,14 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
     return 0;
 }
 
-- (void)setActivityIndicatorViewColor:(UIColor *)activityIndicatorViewColor
-{
-    if ([_activity isKindOfClass:[UIActivityIndicatorView class]] && [_activity respondsToSelector:@selector(setColor:)]) {
-        [(UIActivityIndicatorView *)_activity setColor:activityIndicatorViewColor];
-    }
-}
-
-- (UIColor *)activityIndicatorViewColor
-{
-    if ([_activity isKindOfClass:[UIActivityIndicatorView class]] && [_activity respondsToSelector:@selector(color)]) {
-        return [(UIActivityIndicatorView *)_activity color];
-    }
-    return nil;
-}
-
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
     if ([keyPath isEqualToString:@"contentInset"]) {
         if (!_ignoreInset) {
+            _shapeLayer.opacity = 0;
             self.originalContentInset = [[change objectForKey:@"new"] UIEdgeInsetsValue];
-            self.frame = CGRectMake(0, -(kTotalViewHeight + self.scrollView.contentInset.top), self.scrollView.frame.size.width, kTotalViewHeight);
+            //self.frame = CGRectMake(0, -(kTotalViewHeight + self.scrollView.contentInset.top), self.scrollView.frame.size.width, kTotalViewHeight);
+            self.frame = CGRectMake(0, -(kTotalViewHeight), self.scrollView.frame.size.width, kTotalViewHeight);
         }
         return;
     }
@@ -218,6 +198,11 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
                         [self.scrollView setContentInset:UIEdgeInsetsMake(-offset + self.originalContentInset.top, self.originalContentInset.left, self.originalContentInset.bottom, self.originalContentInset.right)];
                     }
                 }
+                else if(self.scrollView.contentInset.top==self.originalContentInset.top){
+                    CGFloat tmp = self.scrollView.contentOffset.y;
+                    [self.scrollView setContentInset:UIEdgeInsetsMake(kOpenedViewHeight + self.originalContentInset.top, self.originalContentInset.left, self.originalContentInset.bottom, self.originalContentInset.right)];
+                    self.scrollView.contentOffset = CGPointMake(0, tmp - self.scrollView.contentOffset.y - self.scrollView.contentInset.top);
+                }
             } else if (_hasSectionHeaders) {
                 [self.scrollView setContentInset:self.originalContentInset];
             }
@@ -256,7 +241,6 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
             return;
         }
     }
-    
     _lastOffset = offset;
     
     BOOL triggered = NO;
@@ -285,25 +269,48 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         }
     }
     
+    CGFloat bottom2 = bottomOrigin.y;
+    bottomOrigin.y = -(topOrigin.y - bottomOrigin.y)/2 + topOrigin.y;
+    
     //Top semicircle
     CGPathAddArc(path, NULL, topOrigin.x, topOrigin.y, currentTopRadius, 0, M_PI, YES);
     
     //Left curve
-    CGPoint leftCp1 = CGPointMake(lerp((topOrigin.x - currentTopRadius), (bottomOrigin.x - currentBottomRadius), 0.1), lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint leftCp2 = CGPointMake(lerp((topOrigin.x - currentTopRadius), (bottomOrigin.x - currentBottomRadius), 0.9), lerp(topOrigin.y, bottomOrigin.y, 0.2));
+    CGPoint leftCp1 = CGPointMake(lerp((topOrigin.x - currentTopRadius), (bottomOrigin.x - currentBottomRadius), 0.1), lerp(topOrigin.y, bottomOrigin.y, 0.3));
+    CGPoint leftCp2 = CGPointMake(lerp((topOrigin.x - currentTopRadius), (bottomOrigin.x - currentBottomRadius), 0.9), lerp(topOrigin.y, bottomOrigin.y, 0.3));
     CGPoint leftDestination = CGPointMake(bottomOrigin.x - currentBottomRadius, bottomOrigin.y);
     
     CGPathAddCurveToPoint(path, NULL, leftCp1.x, leftCp1.y, leftCp2.x, leftCp2.y, leftDestination.x, leftDestination.y);
     
+    CGPoint leftCp3 = leftCp2;
+    CGPoint leftCp4 = leftCp1;
+    leftCp3.y = 2*bottomOrigin.y - leftCp3.y;
+    leftCp4.y = 2*bottomOrigin.y - leftCp4.y;
+    leftDestination = CGPointMake(topOrigin.x - currentTopRadius, bottom2);
+    
+    CGPathAddCurveToPoint(path, NULL, leftCp3.x, leftCp3.y, leftCp4.x, leftCp4.y, leftDestination.x, leftDestination.y);
+    
     //Bottom semicircle
-    CGPathAddArc(path, NULL, bottomOrigin.x, bottomOrigin.y, currentBottomRadius, M_PI, 0, YES);
+    CGPathAddArc(path, NULL, topOrigin.x, bottom2, currentTopRadius, M_PI, 0, YES);
     
     //Right curve
-    CGPoint rightCp2 = CGPointMake(lerp((topOrigin.x + currentTopRadius), (bottomOrigin.x + currentBottomRadius), 0.1), lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint rightCp1 = CGPointMake(lerp((topOrigin.x + currentTopRadius), (bottomOrigin.x + currentBottomRadius), 0.9), lerp(topOrigin.y, bottomOrigin.y, 0.2));
-    CGPoint rightDestination = CGPointMake(topOrigin.x + currentTopRadius, topOrigin.y);
+    CGPoint rightCp1 = leftCp4;
+    CGPoint rightCp2 = leftCp3;
+    rightCp1.x = 2*bottomOrigin.x - rightCp1.x;
+    rightCp2.x = 2*bottomOrigin.x - rightCp2.x;
+    CGPoint rightDestination = CGPointMake(bottomOrigin.x + currentBottomRadius, bottomOrigin.y);
     
     CGPathAddCurveToPoint(path, NULL, rightCp1.x, rightCp1.y, rightCp2.x, rightCp2.y, rightDestination.x, rightDestination.y);
+    
+    CGPoint rightCp3 = rightCp2;
+    CGPoint rightCp4 = rightCp1;
+    
+    rightCp3.y = 2*bottomOrigin.y - rightCp3.y;
+    rightCp4.y = 2*bottomOrigin.y - rightCp4.y;
+    rightDestination = CGPointMake(topOrigin.x + currentTopRadius, topOrigin.y);
+    
+    CGPathAddCurveToPoint(path, NULL, rightCp3.x, rightCp3.y, rightCp4.x, rightCp4.y, rightDestination.x, rightDestination.y);
+    
     CGPathCloseSubpath(path);
     
     if (!triggered) {
@@ -311,58 +318,69 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         
         _shapeLayer.path = path;
         _shapeLayer.shadowPath = path;
-        
-        // Add the arrow shape
-        
-        CGFloat currentArrowSize = lerp(kMinArrowSize, kMaxArrowSize, percentage);
-        CGFloat currentArrowRadius = lerp(kMinArrowRadius, kMaxArrowRadius, percentage);
-        CGFloat arrowBigRadius = currentArrowRadius + (currentArrowSize / 2);
-        CGFloat arrowSmallRadius = currentArrowRadius - (currentArrowSize / 2);
-        CGMutablePathRef arrowPath = CGPathCreateMutable();
-        CGPathAddArc(arrowPath, NULL, topOrigin.x, topOrigin.y, arrowBigRadius, 0, 3 * M_PI_2, NO);
-        CGPathAddLineToPoint(arrowPath, NULL, topOrigin.x, topOrigin.y - arrowBigRadius - currentArrowSize);
-        CGPathAddLineToPoint(arrowPath, NULL, topOrigin.x + (2 * currentArrowSize), topOrigin.y - arrowBigRadius + (currentArrowSize / 2));
-        CGPathAddLineToPoint(arrowPath, NULL, topOrigin.x, topOrigin.y - arrowBigRadius + (2 * currentArrowSize));
-        CGPathAddLineToPoint(arrowPath, NULL, topOrigin.x, topOrigin.y - arrowBigRadius + currentArrowSize);
-        CGPathAddArc(arrowPath, NULL, topOrigin.x, topOrigin.y, arrowSmallRadius, 3 * M_PI_2, 0, YES);
-        CGPathCloseSubpath(arrowPath);
-        _arrowLayer.path = arrowPath;
-        [_arrowLayer setFillRule:kCAFillRuleEvenOdd];
-        CGPathRelease(arrowPath);
-        
-        // Add the highlight shape
-        
-        CGMutablePathRef highlightPath = CGPathCreateMutable();
-        CGPathAddArc(highlightPath, NULL, topOrigin.x, topOrigin.y, currentTopRadius, 0, M_PI, YES);
-        CGPathAddArc(highlightPath, NULL, topOrigin.x, topOrigin.y + 1.25, currentTopRadius, M_PI, 0, NO);
-        
-        _highlightLayer.path = highlightPath;
-        [_highlightLayer setFillRule:kCAFillRuleNonZero];
-        CGPathRelease(highlightPath);
+        _shapeLayer.opacity = MAX(0, MIN(1, 2*distance/(kOpenedViewHeight+kMinTopPadding)));
         
     } else {
         // Start the shape disappearance animation
         
-        CGFloat radius = lerp(kMinBottomRadius, kMaxBottomRadius, 0.2);
         CABasicAnimation *pathMorph = [CABasicAnimation animationWithKeyPath:@"path"];
         pathMorph.duration = 0.15;
         pathMorph.fillMode = kCAFillModeForwards;
         pathMorph.removedOnCompletion = NO;
+        
+        CGMutablePathRef fromPath = CGPathCreateMutable();
+        CGMutablePathRef tmpPath = CGPathCreateMutable();
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, topOrigin.y, currentTopRadius, 0, M_PI, YES);
+        leftDestination = CGPointMake(bottomOrigin.x - currentBottomRadius, bottomOrigin.y-5);
+        CGPathAddCurveToPoint(tmpPath, NULL, leftCp1.x, leftCp1.y, leftCp2.x, leftCp2.y, leftDestination.x, leftDestination.y);
+        CGPathAddArc(tmpPath, NULL, bottomOrigin.x, bottomOrigin.y-5, currentBottomRadius, M_PI, 0, YES);
+        rightDestination = CGPointMake(topOrigin.x + currentTopRadius, topOrigin.y);
+        CGPathAddCurveToPoint(tmpPath, NULL, rightCp3.x, rightCp3.y, rightCp4.x, rightCp4.y, rightDestination.x, rightDestination.y);
+        CGPathCloseSubpath(tmpPath);
+        CGPathAddPath(fromPath, &CGAffineTransformIdentity, tmpPath);
+        CGPathRelease(tmpPath);
+        
+        tmpPath = CGPathCreateMutable();
+        CGPathAddArc(tmpPath, NULL, bottomOrigin.x, bottomOrigin.y+5, currentBottomRadius, 0, M_PI, YES);
+        leftDestination = CGPointMake(topOrigin.x - currentTopRadius, bottom2);
+        CGPathAddCurveToPoint(tmpPath, NULL, leftCp3.x, leftCp3.y+5, leftCp4.x, leftCp4.y+5, leftDestination.x, leftDestination.y);
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, bottom2, currentTopRadius, M_PI, 0, YES);
+        rightDestination = CGPointMake(bottomOrigin.x + currentBottomRadius, bottomOrigin.y+5);
+        CGPathAddCurveToPoint(tmpPath, NULL, rightCp1.x, rightCp1.y+5, rightCp2.x, rightCp2.y+5, rightDestination.x, rightDestination.y);
+        CGPathCloseSubpath(tmpPath);
+        CGPathAddPath(fromPath, &CGAffineTransformIdentity, tmpPath);
+        CGPathRelease(tmpPath);
+        
+        pathMorph.fromValue = (__bridge id)fromPath;
+        
+        CGFloat radius = 3;
         CGMutablePathRef toPath = CGPathCreateMutable();
-        CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, 0, M_PI, YES);
-        CGPathAddCurveToPoint(toPath, NULL, topOrigin.x - radius, topOrigin.y, topOrigin.x - radius, topOrigin.y, topOrigin.x - radius, topOrigin.y);
-        CGPathAddArc(toPath, NULL, topOrigin.x, topOrigin.y, radius, M_PI, 0, YES);
-        CGPathAddCurveToPoint(toPath, NULL, topOrigin.x + radius, topOrigin.y, topOrigin.x + radius, topOrigin.y, topOrigin.x + radius, topOrigin.y);
-        CGPathCloseSubpath(toPath);
+        
+        tmpPath = CGPathCreateMutable();
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, topOrigin.y, radius, 0, M_PI, YES);
+        CGPathAddCurveToPoint(tmpPath, NULL, topOrigin.x - radius, topOrigin.y, topOrigin.x - radius, topOrigin.y, topOrigin.x - radius, topOrigin.y);
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, topOrigin.y, radius, M_PI, 0, YES);
+        CGPathAddCurveToPoint(tmpPath, NULL, topOrigin.x + radius, topOrigin.y, topOrigin.x + radius, topOrigin.y, topOrigin.x + radius, topOrigin.y);
+        CGPathCloseSubpath(tmpPath);
+        CGPathAddPath(toPath, &CGAffineTransformIdentity, tmpPath);
+        CGPathRelease(tmpPath);
+        
+        tmpPath = CGPathCreateMutable();
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, topOrigin.y+15, radius, 0, M_PI, YES);
+        CGPathAddCurveToPoint(tmpPath, NULL, topOrigin.x - radius, topOrigin.y+15, topOrigin.x - radius, topOrigin.y+15, topOrigin.x - radius, topOrigin.y+15);
+        CGPathAddArc(tmpPath, NULL, topOrigin.x, topOrigin.y+15, radius, M_PI, 0, YES);
+        CGPathAddCurveToPoint(tmpPath, NULL, topOrigin.x + radius, topOrigin.y+15, topOrigin.x + radius, topOrigin.y+15, topOrigin.x + radius, topOrigin.y+15);
+        CGPathCloseSubpath(tmpPath);
+        CGPathAddPath(toPath, &CGAffineTransformIdentity, tmpPath);
+        CGPathRelease(tmpPath);
+        
         pathMorph.toValue = (__bridge id)toPath;
+        
         [_shapeLayer addAnimation:pathMorph forKey:nil];
-        CABasicAnimation *shadowPathMorph = [CABasicAnimation animationWithKeyPath:@"shadowPath"];
-        shadowPathMorph.duration = 0.15;
-        shadowPathMorph.fillMode = kCAFillModeForwards;
-        shadowPathMorph.removedOnCompletion = NO;
-        shadowPathMorph.toValue = (__bridge id)toPath;
-        [_shapeLayer addAnimation:shadowPathMorph forKey:nil];
+        
+        CGPathRelease(fromPath);
         CGPathRelease(toPath);
+        
         CABasicAnimation *shapeAlphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
         shapeAlphaAnimation.duration = 0.1;
         shapeAlphaAnimation.beginTime = CACurrentMediaTime() + 0.1;
@@ -370,18 +388,14 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         shapeAlphaAnimation.fillMode = kCAFillModeForwards;
         shapeAlphaAnimation.removedOnCompletion = NO;
         [_shapeLayer addAnimation:shapeAlphaAnimation forKey:nil];
-        CABasicAnimation *alphaAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
-        alphaAnimation.duration = 0.1;
-        alphaAnimation.toValue = [NSNumber numberWithFloat:0];
-        alphaAnimation.fillMode = kCAFillModeForwards;
-        alphaAnimation.removedOnCompletion = NO;
-        [_arrowLayer addAnimation:alphaAnimation forKey:nil];
-        [_highlightLayer addAnimation:alphaAnimation forKey:nil];
+        
         
         [CATransaction begin];
         [CATransaction setValue:(id)kCFBooleanTrue forKey:kCATransactionDisableActions];
         _activity.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1);
         [CATransaction commit];
+        
+        [_activity performSelectorOnMainThread:@selector(startAnimating) withObject:nil waitUntilDone:NO];
         [UIView animateWithDuration:0.2 delay:0.15 options:UIViewAnimationOptionCurveLinear animations:^{
             _activity.alpha = 1;
             _activity.layer.transform = CATransform3DMakeScale(1, 1, 1);
@@ -409,6 +423,7 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         
         _activity.alpha = 1;
         _activity.layer.transform = CATransform3DMakeScale(1, 1, 1);
+        [_activity performSelectorOnMainThread:@selector(startAnimating) withObject:nil waitUntilDone:NO];
 
         CGPoint offset = self.scrollView.contentOffset;
         _ignoreInset = YES;
@@ -429,14 +444,37 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
         // halfway through the end animation.
         // This allows for the refresh control to clean up the observer,
         // in the case the scrollView is released while the animation is running
-        __block UIScrollView *blockScrollView = self.scrollView;
-        [UIView animateWithDuration:0.4 animations:^{
-            _ignoreInset = YES;
-            [blockScrollView setContentInset:self.originalContentInset];
-            _ignoreInset = NO;
+        
+        if(!self.scrollView.isTracking){
+            __block UIScrollView *blockScrollView = self.scrollView;
+            [UIView animateWithDuration:0.4 animations:^{
+                _ignoreInset = YES;
+                [blockScrollView setContentInset:self.originalContentInset];
+                _ignoreInset = NO;
+                _activity.alpha = 0;
+                _activity.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1);
+            } completion:^(BOOL finished) {
+                [_shapeLayer removeAllAnimations];
+                _shapeLayer.path = nil;
+                _shapeLayer.shadowPath = nil;
+                _shapeLayer.position = CGPointZero;
+                [_arrowLayer removeAllAnimations];
+                _arrowLayer.path = nil;
+                [_highlightLayer removeAllAnimations];
+                _highlightLayer.path = nil;
+                // We need to use the scrollView somehow in the end block,
+                // or it'll get released in the animation block.
+                _ignoreInset = YES;
+                [blockScrollView setContentInset:self.originalContentInset];
+                _ignoreInset = NO;
+                
+                [_activity performSelectorOnMainThread:@selector(stopAnimating) withObject:nil waitUntilDone:NO];
+            }];
+        }
+        else{
             _activity.alpha = 0;
             _activity.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1);
-        } completion:^(BOOL finished) {
+            
             [_shapeLayer removeAllAnimations];
             _shapeLayer.path = nil;
             _shapeLayer.shadowPath = nil;
@@ -445,12 +483,16 @@ static inline CGFloat lerp(CGFloat a, CGFloat b, CGFloat p)
             _arrowLayer.path = nil;
             [_highlightLayer removeAllAnimations];
             _highlightLayer.path = nil;
-            // We need to use the scrollView somehow in the end block,
-            // or it'll get released in the animation block.
-            _ignoreInset = YES;
-            [blockScrollView setContentInset:self.originalContentInset];
-            _ignoreInset = NO;
-        }];
+            
+            if(self.scrollView.contentInset.top != self.originalContentInset.top){
+                _ignoreInset = YES;
+                CGFloat tmp = self.scrollView.contentOffset.y;
+                [self.scrollView setContentInset:self.originalContentInset];
+                self.scrollView.contentOffset = CGPointMake(0, tmp - self.scrollView.contentOffset.y - self.scrollView.contentInset.top);
+                _ignoreInset = NO;
+            }
+            [_activity performSelectorOnMainThread:@selector(stopAnimating) withObject:nil waitUntilDone:NO];
+        }
     }
 }
 
